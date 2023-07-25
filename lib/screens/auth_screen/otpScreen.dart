ff7a3fdac1f61d63c3a28ffbc0431bc7d0bc2fb0
Esevoka Onoyovwere <sevoyoung1@gmail.com>
2023-07-25 23:16:27 +0100

    Rework on otp page

--- lib/screens/auth_screen/otpScreen.dart
+++ lib/screens/auth_screen/otpScreen.dart
@@ -1,11 +1,19 @@
 import 'package:fintech_app/screens/auth_screen/userDetailsScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+String otpValues = '';
 
 class OtpScreen extends StatelessWidget {
   const OtpScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
+    TextEditingController otp1 = TextEditingController();
+    TextEditingController otp2 = TextEditingController();
+    TextEditingController otp3 = TextEditingController();
+    TextEditingController otp4 = TextEditingController();
+    TextEditingController otp5 = TextEditingController();
+    TextEditingController otp6 = TextEditingController();
     var size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
@@ -22,48 +30,268 @@ class OtpScreen extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      const SizedBox(height: 30),
-                      SizedBox(
-                        width: size.width * .7,
-                        child: const Text(
-                          'Email OTP',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 3,
-                            height: 1.5,
-                            fontSize: 15,
-                          ),
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: size.width * .7,
+                              child: const Text(
+                                'Email OTP',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 3,
+                                  height: 1.5,
+                                  fontSize: 15,
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 5),
-                      SizedBox(
-                        height: 55,
-                        width: size.width * .7,
-                        child: TextFormField(
-                          textAlign: TextAlign.center,
-                          keyboardType: TextInputType.number,
-                          textInputAction: TextInputAction.go,
-                          autofillHints: const [AutofillHints.oneTimeCode],
-                          decoration: const InputDecoration(
-                            floatingLabelStyle:
-                                TextStyle(color: Colors.blueGrey),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey,
+                      const SizedBox(height: 10),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                controller: otp1,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                // autofillHints: const [
+                                //   AutofillHints.oneTimeCode
+                                // ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
                               ),
                             ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.black,
-                                width: 2.0,
+                            const SizedBox(width: 5),
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                controller: otp2,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                // autofillHints: const [
+                                //   AutofillHints.oneTimeCode
+                                // ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
                               ),
                             ),
-                            //border: OutlineInputBorder(),
-                          ),
+                            const SizedBox(width: 5),
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                controller: otp3,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                // autofillHints: const [
+                                //   AutofillHints.oneTimeCode
+                                // ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                controller: otp4,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                // autofillHints: const [
+                                //   AutofillHints.oneTimeCode
+                                // ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                controller: otp5,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                // autofillHints: const [
+                                //   AutofillHints.oneTimeCode
+                                // ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 5),
+                            SizedBox(
+                              height: 45,
+                              width: 45,
+                              child: TextFormField(
+                                showCursor: false,
+                                controller: otp6,
+                                textAlign: TextAlign.center,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                decoration: const InputDecoration(
+                                  hintText: '0',
+                                  floatingLabelStyle:
+                                      TextStyle(color: Colors.blueGrey),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Colors.black,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                  //border: OutlineInputBorder(),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
 
-                      const SizedBox(height: 20),
+                      const SizedBox(height: 40),
 
                       //Next Button
                       SizedBox(
@@ -76,6 +304,13 @@ class OtpScreen extends StatelessWidget {
                                 minimumSize: const Size.fromHeight(50),
                               ),
                               onPressed: () {
+                                otpValues = otp1.text +
+                                    otp2.text +
+                                    otp3.text +
+                                    otp4.text +
+                                    otp5.text +
+                                    otp6.text;
+                                print('the otp is $otpValues');
                                 Navigator.push(
                                   context,
                                   MaterialPageRoute(
@@ -97,7 +332,8 @@ class OtpScreen extends StatelessWidget {
                         child: const Text(
                           'Check the inbox, promotion and spam/junk folder of your email',
                           style: TextStyle(
-                              color: Colors.grey, fontWeight: FontWeight.bold),
+                              color: Colors.blueGrey,
+                              fontWeight: FontWeight.bold),
                         ),
                       ),
                     ],
